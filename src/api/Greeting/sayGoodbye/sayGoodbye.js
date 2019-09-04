ea7f271075e9e9a4f15b890c6e756051e177e893
tonky0110 <tonky0110@gmail.com>
2019-09-04 22:59:20 +0900

    2.3 intergrating Prisma in our server

--- src/api/Greeting/sayGoodbye/sayGoodbye.js
+++ src/api/Greeting/sayGoodbye/sayGoodbye.js
@@ -1,5 +1,9 @@
+import { prisma } from '../../../../generated/prisma-client';
 export default {
 	Query: {
-		sayGoodbye: () => 'Good bye'
+		sayGoodbye: async () => {
+			console.log(await prisma.users());
+			return 'HELLO';
+		}
 	}
 };
