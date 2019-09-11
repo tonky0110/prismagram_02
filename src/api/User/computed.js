a2d4d4eaaa98fec2485bd075671f33d8a9f795a6
tonky0110 <tonky0110@gmail.com>
2019-09-11 21:02:52 +0900

    3.16 itsMe --> isSelf, amIFollwing --> isFollwing으로 변경 및 버그 수정

---
+++ src/api/User/computed.js
@@ -0,0 +1,28 @@
+import { prisma } from '../../../generated/prisma-client';
+
+export default {
+	User: {
+		fullName: (parent) => {
+			return `${parent.firstName} ${parent.lastName}`;
+		},
+		isFollowing: async (parent, _, { request }) => {
+			const { user } = request;
+			const { id: parentId } = parent;
+			try {
+				console.log('parent: ', parent);
+				console.log('user: ', user);
+				return prisma.$exists.user({
+					AND: [ { id: user.id }, { following_some: { id: parentId } } ]
+				});
+			} catch (error) {
+				console.log(error);
+				return false;
+			}
+		},
+		isSelf: (parent, _, { request }) => {
+			const { user } = request;
+			const { id: parentId } = parent;
+			return user.id === parentId;
+		}
+	}
+};
