472510590d2d2af6508e22bc1b38a20d5bb4de2a
tonky0110 <tonky0110@gmail.com>
2019-09-15 16:08:05 +0900

    3.19 refactoring seeFullPost

--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,28 @@
+export const USER_FRAGMENT = `
+    id
+    username
+`;
+
 export const COMMENT_FRAGMENT = `
-    fragment CommentFragment on Comment {
+    id
+    text
+    user {
+        ${USER_FRAGMENT}
+    }
+`;
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostFragment on Post {
         id
-        text
-        user {
-            username
-        }
+        location
+        caption
+        files{${FILE_FRAGMENT}}
+        comments{${COMMENT_FRAGMENT}}
+        user{${USER_FRAGMENT}}
     }
 `;
